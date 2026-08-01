01a02d0acb5ae65ddfd20cb207bf312840548099
sudheer <sudheerachari694@gmail.com>
2026-08-01 15:40:32 +0530

    Main bar dynamic grid is updated

---
+++ sub-navs.js
@@ -0,0 +1,20 @@
+const tabButtons = document.querySelectorAll('.tab-btn');
+const collapseMain = document.querySelector('.main');
+const homeButton = document.querySelector('.home');
+const tabPanel = document.querySelectorAll('.tab-panel');
+
+tabButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    tabButtons.forEach(btn => btn.classList.remove('active'));
+    tabPanel.forEach(panel => panel.classList.remove('active'));
+
+    button.classList.add('active');
+    const activeTab = document.getElementById(button.dataset.tab).classList.add('active');
+
+    collapseMain.classList.add('active');
+  });
+});
+
+homeButton.addEventListener('click', () => {
+  collapseMain.classList.remove('active');
+});
